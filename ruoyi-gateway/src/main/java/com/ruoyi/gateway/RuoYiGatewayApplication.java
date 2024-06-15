896756720b9f05f3677b8c8ca75a5a1957ce1266
方启军 <2997855025@qq.com>
2024-06-15 16:27:57 +0800

    实现登录和退出

--- ruoyi-gateway/src/main/java/com/ruoyi/gateway/RuoYiGatewayApplication.java
+++ ruoyi-gateway/src/main/java/com/ruoyi/gateway/RuoYiGatewayApplication.java
@@ -25,5 +25,6 @@ public class RuoYiGatewayApplication
                 " |  | \\ `'   /|   `-'  /           \n" +
                 " |  |  \\    /  \\      /           \n" +
                 " ''-'   `'-'    `-..-'              ");
+
     }
 }
